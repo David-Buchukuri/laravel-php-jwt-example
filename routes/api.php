acd01a3de21ee60a20b35725560fde0891c18796
David-Buchukuri <datobuchukuri20@gmail.com>
2022-11-05 20:03:55 +0400

    feat: return jwt token as http only cookie on successful login

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Firebase\JWT\JWT;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,3 +14,28 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+Route::post('/login', function () {
+    $autheticated = auth()->attempt(
+        [
+            'email' => request()->email,
+            'password' => request()->password,
+        ]
+    );
+
+    if (!$autheticated) {
+        return response()->json('wrong email or password', 401);
+    }
+
+    $payload = [
+        'issued_at' => Carbon::now()->timestamp,
+        'valid_till' => Carbon::now()->addMinute(30)->timestamp,
+    ];
+
+    $jwt = JWT::encode($payload, config('auth.jwt_secret'), 'HS256');
+
+    $cookie = cookie("access_token", $jwt, 30, '/', config('auth.front_end_top_level_domain'), true, true, false, 'None');
+
+    return response()->json('success', 200)->withCookie($cookie);
+});
